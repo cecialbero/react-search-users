e7ab3de95f6b912adfe730a6254c259e576c3741
Cecilia Albero <cecialbero@Cecilias-MacBook-Air.local>
2021-12-16 16:40:16 -0300

    Create table styles

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,26 +1,34 @@
 import React, { useContext } from 'react';
 import UsersContext from './../context/Users/usersContext';
 
+import './Results.scss';
+
 const Results = () => {
     const usersContext = useContext(UsersContext);
     const { users } = usersContext;
 
+    console.log(users)
+
     return (
-        <table>
-            <tbody>
+        <>
             {
-                users.map(user => {
-                    return (
-                        <tr key={user.id}>
-                            <td>{user.avatar_url}</td>
-                            <td>{user.login}</td>
-                            <td>{user.type}</td>
-                        </tr>
-                    )
-                })
+                users.length != 0 && <table>
+                    <tbody>
+                    {
+                        users.map(user => {
+                            return (
+                                <tr key={user.id}>
+                                    <td>{user.avatar_url}</td>
+                                    <td>{user.login}</td>
+                                    <td>{user.type}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </table>
             }
-            </tbody>
-        </table>
+        </>
     )
 }
 
