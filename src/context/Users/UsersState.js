8b0cb481554647a7a212af1eb6aa9d0ad84b2e4d
cecialbero <ceci.albero@gmail.com>
2021-12-17 15:56:42 -0300

    Create and add the client id and client secret id

--- src/context/Users/UsersState.js
+++ src/context/Users/UsersState.js
@@ -1,9 +1,9 @@
 import React, { useReducer } from 'react';
 import axios from 'axios';
+import { Octokit, App } from "octokit";
 import { GET_USERS } from './../types';
 import UsersContext from './usersContext';
 import UsersReducer from './usersReducer';
-
 
 const UsersState = props => {
     const initialState = {
@@ -13,7 +13,10 @@ const UsersState = props => {
     const [state, dispatch] = useReducer(UsersReducer, initialState);
 
     const getUsers = async username => {
-        const res = await axios.get(`https://api.github.com/search/users?q=${username} in:login`);
+        const octokit = new Octokit({ auth: `personal-access-token123` });
+
+        const res = await axios.get(`https://api.github.com/search/users?q=${username}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET} in:login`);
+        
         dispatch({ type: GET_USERS, payload: res.data.items });
     }
 
