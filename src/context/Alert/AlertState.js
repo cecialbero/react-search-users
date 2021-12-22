7dba659d6b14b4ecd6938e7678fa42457b894310
cecialbero <ceci.albero@gmail.com>
2021-12-22 15:42:06 -0300

    Create remove_alert option on alert reducer

--- src/context/Alert/AlertState.js
+++ src/context/Alert/AlertState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 import AlertContext from './alertContext';
 import AlertReducer from './alertReducer';
-import { SET_ALERT } from './../types';
+import { SET_ALERT, REMOVE_ALERT } from './../types';
 
 const AlertState = props => {
     const initialState = null;
@@ -10,6 +10,9 @@ const AlertState = props => {
 
     const setAlert = (msg, type) => {
         dispatch({ type: SET_ALERT, payload: { msg, type }})
+        setTimeout(() => {
+            dispatch({ type: REMOVE_ALERT })
+        }, 3000)
     }
 
     return <AlertContext.Provider value={{
--- src/context/types.js
+++ src/context/types.js
@@ -1,3 +1,4 @@
 export const GET_USERS = 'GET_USERS';
 export const LOADING = 'LOADING';
 export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
