3a22895ca44af7c962e6cf6f35bad6eed6ee8e91
cecialbero <ceci.albero@gmail.com>
2021-12-25 22:06:22 -0300

    Set a item limit per page

--- src/components/Results.js
+++ src/components/Results.js
@@ -55,48 +55,51 @@ const Results = () => {
         previousPage,
         pageOptions,
         state: { pageIndex },
-    } = useTable({ columns, data }, useSortBy, usePagination)
+    } = useTable({ columns, data, initialState: { pageSize: 9 } }, useSortBy, usePagination)
+
+    console.log(1, canNextPage)
+    console.log(2, canPreviousPage)
 
     return (
 
         loading ? <div className='spinner-wrapper'><Spinner /></div>
 
-            :
+        :
 
-            users.length !== 0 && <div className='table-wrapper' role='table' arial-label='results table'>
-                <table {...getTableProps()}>
-                    <thead>
-                        {headerGroups.map(headerGroup => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map(column => (
-                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                        <p>{column.render('Header')}
-                                            {column.isSorted
-                                                ? column.isSortedDesc
-                                                    ? <i className="fas fa-sort-down"></i>
-                                                    : <i className="fas fa-sort-up"></i>
-                                                : ''}
-                                        </p>
-                                    </th>
-                                ))}
+        users.length !== 0 && <div className='table-wrapper' role='table' arial-label='results table'>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                    <p>{column.render('Header')}
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? <i className="fas fa-sort-down"></i>
+                                                : <i className="fas fa-sort-up"></i>
+                                            : ''}
+                                    </p>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map((row) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })}
                             </tr>
-                        ))}
-                    </thead>
-                    <tbody {...getTableBodyProps()}>
-                        {page.map((row) => {
-                            prepareRow(row)
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map(cell => {
-                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                    })}
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
+                        )
+                    })}
+                </tbody>
+            </table>
 
-                <div className="pagination">
+            { (canNextPage || canPreviousPage) && <div className="pagination">
                     <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
                         {'<<'}
                     </button>{' '}
@@ -116,7 +119,8 @@ const Results = () => {
                         </strong>{' '}
                     </span>
                 </div>
-            </div>
+            }
+        </div>
     )
 }
 
